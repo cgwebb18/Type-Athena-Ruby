92b41690777f2e9f4f302c29813c76a88b29dadb
Webb <webbc01@LAP-APP-C02PMB7SFVH5.local>
2019-01-22 13:34:20 -0500

    made it so that the user does not have to turn off their Character Accent Selection, somehow wrapping the keyboard in a function accomplished this

--- ARKeyboard.js
+++ ARKeyboard.js
@@ -1,47 +1,22 @@
- $(document).ready(function() {
-  $('#main_keyboard').keyboard({
-      layout: 'AR-Latin',
-      usePreview: false,
-      acceptValid: true,
-      validate: function (kb, val) {
-          return val.length > 3;
-      }
-  })
-  // activate the typing extension
-  .addTyping({
-      showTyping: true,
-      delay: 250
-  }).previewKeyset({
-    'sets': ['normal', 'shift']
-  }).addAltKeyPopup({
-    // time to hold down a button in milliseconds to trigger a popup
-    holdTime : 500,
-    // events triggered when popup is visible & hidden
-    popupVisible  : 'popup-visible',
-    popupHidden   : 'popup-hidden',
-    // optional reposition popup callback function
-    popupPosition : function(keyboard, data) {
-      console.log(data);
-      /*
-      {
-        $kb         : Keyboard element (jQuery object),
-        $key        : Clicked key element (jQuery object),
-        $popup      : Popup container (jQuery object),
-        kbHeight    : Keyboard element outer height,
-        kbWidth     : Keyboard element outer width,
-        popupHeight : Popup container height,
-        popupWidth  : Popup container width,
-        popupLeft   : Popup container left position,
-        popupTop    : Popup container top position
-      }
-      example (shift popup left 100px):
-      data.$popup.css('left', data.popupLeft - 100);
-      */
-    }
-  });
-
-  console.log($.keyboard.altKeys)
-});
+$(function(){
+		$('#main_keyboard').keyboard({
+			layout: 'AR-Latin',
+			repeatRate : 0
+		}).addTyping({
+        showTyping: true,
+        delay: 250
+    }).previewKeyset({
+      'sets': ['normal', 'shift']
+    }).addAltKeyPopup({
+			// time to hold down a button in ms to trigger a popup
+			holdTime : 500,
+			// event triggered when popup is visible
+			// access the overlay from keyboard.altKeyPopup_$overlay
+			// or the keys container from keyboard.altKeyPopup_$overlay.find('.ui-keyboard-popup')
+			// or the keys from keyboard.altKeyPopup_$overlay.find('.ui-keyboard-button')
+			popupVisible : 'popup-visible'
+		});
+	});
 
 
 
