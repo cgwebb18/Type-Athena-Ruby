48f3dd2386bedc1459bd29861dcd665829274d71
Webb <webbc01@LAP-APP-C02PMB7SFVH5.local>
2019-01-22 15:04:27 -0500

    testing

---
+++ layouts/AR_altKeys.js
@@ -0,0 +1,25 @@
+var AR_altKeys =
+{
+  a : '\ue300 \ue301 \ue302 \ue303 \ue304 \ue305',
+  b : '\ue306',
+  c : '\ue307 \ue308 \ue309 \ue30a',
+  d : '\ue30b \ue30c \ue30d \ue30e \ue30f \ue310 \ue311 \ue312 \ue313',
+  e : '\ue314 \ue315 \ue316 \ue317',
+  f : '\ue318 \ue319 \ue31a',
+  g : '\ue31b \ue31c \ue31d \ue31e \ue31f \ue320 \ue321 \ue322 \ue323',
+  h : '\ue324 \ue325',
+  i : '\ue326',
+  k : '\ue327',
+  l : '\ue328 \ue329 \ue32a',
+  m : '\ue32a\b \ue32a\c \ue32d \ue32e \ue32f \ue330 \ue331',
+  n : '\ue332 \ue333 \ue334 \ue335 \ue336',
+  o : '\ue337',
+  p : '\ue338',
+  r : '\ue339 \ue33a \ue33b \ue33c \ue33d',
+  s : '\ue33e \ue33f \ue340',
+  t : '\ue341 \ue342 \ue343 \ue344',
+  u : '\ue345 \ue346 \ue347 \ue348',
+  v : '\ue349 \ue34a',
+  z : '\ue34b'
+
+}
