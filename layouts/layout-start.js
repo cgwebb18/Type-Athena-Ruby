c903d375c9ec17a5ffa5504136c999d653b49981
Webb <webbc01@LAP-APP-C02PMB7SFVH5.local>
2019-02-06 14:31:14 -0500

    fixed greek kb backspace issue

--- layouts/layout-start.js
+++ layouts/layout-start.js
@@ -18,7 +18,7 @@ jQuery.keyboard.layouts['AR_L'] = {
 jQuery.keyboard.layouts['AR_G'] = {
 	'name' : 'AR_G',
 	'normal' : [
-		"\u03d8(1) \u03e0(2) 3 4 5 \u00b7(6) \u00b7(7) \u02d9(8) \u0323(9) \u0305(0) \ue400(-) \u2e09(=){bksp}",
+		"\u03d8(1) \u03e0(2) 3 4 5 \u00b7(6) \u00b7(7) \u02d9(8) \u0323(9) \u0305(0) \ue400(-) \u2e09(=) {bksp}",
 		"{tab} ;(q) \u03da(w) \u0395(e) \u03a1(r) \u03a4(t) \u03a5(y) \u0398(u) \u0399(i) \u039f(o) \u03a0(p) \u005b([) \u005d(])",
 		"\u0391(a) \u03a3(s) \u0393(d) \u03a6(f) \u0393(g) \u0397(h) \u039e(j) \u039a(k) \u039b(l) \u007c(;) {enter}",
 		"{shift} \u0396(z) \u03a7(x) \u03a8(c) \u03a9(v) \u0392(b) \u039d(n) \u039c(m) , . / {shift}",
