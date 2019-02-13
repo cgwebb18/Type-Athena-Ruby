a52c28bd1914310d96bdcb177103b835e7b13788
Webb <webbc01@LAP-APP-C02PMB7SFVH5.local>
2019-02-13 16:23:09 -0500

    deleted some unnecessary stuff

--- layouts/layout-start.js
+++ layouts/layout-start.js
@@ -34,27 +34,6 @@ jQuery.keyboard.layouts['AR_G'] = {
 };
 
 
-// jQuery.keyboard.layouts['Extender1'] = {
-// 	'name' : 'Extender1',
-// 	'normal' : [
-// 		'\ue180 \ue181 \ue182 \ue183 \ue184 \ue185 \ue186 \ue187 \ue188 \ue189 \ue18a \ue18b',
-// 		'\ue18c \ue18d \ue18e \ue18f \ue190 \ue191 \ue192 \ue193 \ue194 \ue195 \ue196 \ue197',
-// 		'\ue198 \ue199 \ue19a \ue19b \ue19c \ue19d \ue19e \ue19f \ue1a0 \ue1a1 \ue1a2 \ue1a3',
-// 		'\ue1a4 \ue1a5 \ue1a6 \ue1a7 \ue1a8 \ue1a9 \ue1aa \ue1ab \ue1ac \ue1ad \ue1ae \ue1af',
-// 		'\ue1b0 \ue1b1 \ue1b2 \ue1b3 \ue1b4 \ue1b5 \ue1b6 \ue1b7 \ue1b8 \ue1b9 \ue1ba \ue1bb',
-// 		'\ue1bb \ue1bc \ue1bd \ue1be \ue1bf \ue1c0 \ue1c1 \ue1c2 \ue1c3 \ue1c4 \ue1c5 \ue1c6',
-// 		'\ue1c7 \ue1c8 \ue1c9 \ue1ca \ue1cb \ue1cc \ue1cd \ue1ce \ue1cf \ue1d0 \ue1d1 \ue1d2',
-// 		'\ue1d3 \ue1d4 \ue1d5 \ue1d6 \ue1d7 \ue1d8 \ue1d9 \ue1da \ue1db \ue1dc \ue1dd \ue1df',
-// 		'\ue1e0 \ue1e1 \ue1e2 \ue1e4 \ue1e5 \ue1e6 \ue1e7 \ue1e8 \ue1e9 \ue1ea \ue1eb \ue1ec',
-// 		'\ue1ed \ue1ef \ue1f0 \ue1f1 \ue1f2 \ue1f3 \ue1f4 \ue1f5 \ue1f6 \ue1f7 \ue1f8 \ue1f9',
-// 		'\ue1fa \ue1fb \ue1fc \ue1fd \ue1fe \ue1ff \ue200 \ue201 \ue202 \ue203 \ue204 \ue205',
-// 		'\ue206 \ue207 \ue208 \ue209 \ue20a',
-// 		'{test1} {test2} {test3} {test4} {test5}',
-// 		'{test6}'
-// 	]
-// };
-
-
 jQuery.keyboard.layouts['GreekLigatures'] = {
 	'name' : 'GreekLigatures',
 	'normal' : [
